54bc2f2b1d74fccb410de6894b7e5ce23a2d0c4e
Kul-deep-7 <sahiljangid321@gmail.com>
2026-01-19 19:08:33 +0530

    fix bug 2.1

--- Backend/app.js
+++ Backend/app.js
@@ -24,7 +24,7 @@ app.use('/api/v1/todos', todoRoutes)
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/Frontend/todo/dist")));
-  app.get("/*", (req, res) => {
+  app.get("/.*/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "Frontend/todo", "dist", "index.html"));
   });
 }
