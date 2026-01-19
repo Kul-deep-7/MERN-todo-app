0e891bd47d4ec44002bdf592cbba2651a056cd4a
Kul-deep-7 <sahiljangid321@gmail.com>
2026-01-19 18:30:27 +0530

    production

--- Backend/app.js
+++ Backend/app.js
@@ -14,9 +14,9 @@ app.use(express.urlencoded({
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/todo/dist")));
+  app.use(express.static(path.join(__dirname, "/Frontend/todo/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend/todo", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "Frontend/todo", "dist", "index.html"));
   });
 }
 
